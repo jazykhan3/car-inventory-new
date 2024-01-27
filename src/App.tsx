1a170a9f3de1a8870ac3d779bb4bf0538528c526
JahanZaib Sultan <jazykhan3@gmail.com>
2024-01-27 12:00:49 +0500

    build fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import React, { useState } from 'react';
-import './styles/index.css';
-import { carmakers } from './data/car-makers';
-import { carsData } from './data/carsData';
-
-
+import { useState } from "react";
+import "./styles/index.css";
+import { carmakers } from "./data/car-makers";
+import { carsData } from "./data/carsData";
 
 interface Car {
   image: string;
@@ -14,28 +12,27 @@ interface Car {
   engineSize: number; // Assume engineSize is a number property
 }
 
-type EngineSize = "smaller than 2 lit" | "2 to 3 lit" | "greater than 3 lit";
-
-
-
 function App(): JSX.Element {
   const [selectedMaker, setSelectedMaker] = useState(carmakers[0]); // Default to the first maker
   function getRandomEngineSize(): number {
     return Math.random() * 5; // Adjust as needed based on your actual data
   }
-  const handleMakerClick = (maker: typeof carmakers[number]): void => {
+  const handleMakerClick = (maker: (typeof carmakers)[number]): void => {
     setSelectedMaker(maker);
   };
 
   type EngineSize = "smaller than 2 lit" | "2 to 3 lit" | "greater than 3 lit";
 
-  function restructureCarsArray(carsArray: typeof carsData, engine: EngineSize): Car[] {
+  function restructureCarsArray(
+    carsArray: typeof carsData,
+    engine: EngineSize
+  ): Car[] {
     return carsArray
       .map((car: any) => {
-        const titleWords = car.title.split(' ');
+        const titleWords = car.title.split(" ");
         const maker = titleWords.shift();
-        const title = titleWords.join(' ');
-  
+        const title = titleWords.join(" ");
+
         return {
           image: car.image,
           title: title,
@@ -60,45 +57,88 @@ function App(): JSX.Element {
       });
   }
 
-  const restructuredCarsArray: Car[] = restructureCarsArray(carsData,'smaller than 2 lit');
+  const restructuredCarsArray: Car[] = restructureCarsArray(
+    carsData,
+    "smaller than 2 lit"
+  );
 
   return (
     <>
-      <div className='flex'>
+      <div className="flex">
         {/* Sidebar */}
-        <div className='w-1/5 h-screen bg-[#FFFFFF] p-8 flex flex-col gap-8 left-0 fixed overflow-auto'>
-          {carmakers?.map((maker:any, idx:any) => (
+        <div className="w-1/5 h-screen bg-[#FFFFFF] p-8 flex flex-col gap-8 left-0 fixed overflow-auto">
+          {carmakers?.map((maker: any, idx: any) => (
             <div
               key={idx}
               className={`p-5 rounded-lg flex flex-row gap-4 items-center cursor-pointer ${
-                selectedMaker === maker ? 'bg-gray-200' : ''
+                selectedMaker === maker ? "bg-gray-200" : ""
               }`}
               onClick={() => handleMakerClick(maker)}
             >
-              <img src={maker?.urlLogo} width={30} height={30} className='rounded-full' alt={maker?.title} />
-              <span className='text-xl font-normal'>{maker?.title}</span>
+              <img
+                src={maker?.urlLogo}
+                width={30}
+                height={30}
+                className="rounded-full"
+                alt={maker?.title}
+              />
+              <span className="text-xl font-normal">{maker?.title}</span>
             </div>
           ))}
         </div>
 
         {/* Content */}
-        <div className='w-4/5 p-8 gap-8 max-h-fit  flex justify-between flex-wrap absolute right-0 overflow-auto'>
-          {restructuredCarsArray
-            .filter((car) => car?.image && car?.maker?.toLowerCase() === selectedMaker?.title.toLowerCase())
-            .map((car, idx) => (
-              <div key={idx} className='h-max border border-[#e5e5e5] flex flex-col justify-between gap-8 rounded-xl'>
-                <img
-                  src={car?.image}
-                  className='object-contain h-[300px] w-[300px]'
-                  alt={car?.title}
-                  height={300}
-                  width={300}
-                />
-                <div className='flex items-center justify-between p-4'>
-                  <span>{car?.title}</span> <span>{car?.start_production}</span>
+        <div className="w-4/5 p-8  max-h-fit flex-col  flex absolute right-0 overflow-auto">
+          <div className="w-100 h-[80px] mb-5 relative rounded-lg">
+            <input
+              type="text"
+              placeholder="Search..."
+              className="w-full h-full pl-10 pr-4 rounded-lg border border-gray-300 focus:outline-none"
+            />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              className="absolute left-3 top-[37%] h-5 w-5 text-gray-400"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M21 21l-5.2-5.2"
+              />
+              <circle cx="10" cy="10" r="7" />
+            </svg>
+          </div>{" "}
+          <div className="flex flex-row gap-8 flex-wrap  justify-between ">
+            {" "}
+            {restructuredCarsArray
+              .filter(
+                (car) =>
+                  car?.image &&
+                  car?.maker?.toLowerCase() ===
+                    selectedMaker?.title.toLowerCase()
+              )
+              .map((car, idx) => (
+                <div
+                  key={idx}
+                  className="h-max border border-[#e5e5e5] flex flex-col justify-between gap-8 rounded-xl"
+                >
+                  <img
+                    src={car?.image}
+                    className="object-contain h-[300px] w-[300px]"
+                    alt={car?.title}
+                    height={300}
+                    width={300}
+                  />
+                  <div className="flex items-center justify-between p-4">
+                    <span>{car?.title}</span>{" "}
+                    <span>{car?.start_production}</span>
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
+          </div>
         </div>
       </div>
     </>
